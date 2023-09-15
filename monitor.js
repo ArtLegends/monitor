6899aa47bfdc20c4d46c366665277262250207ee
ArtLegends <norik.artemka@gmail.com>
2023-09-15 23:09:29 +0300

    .

--- monitor.js
+++ monitor.js
@@ -1,3 +1,3 @@
 function redirectToPreorder() {
-    window.location.href = "https://th-store.org/products/asus-rog-swift-oled-pg48uq";
+    window.location.href = "https://tm-market.com/en/products/asus-rog-swift-oled-pg48uq";
 }
