baccf3c74bb90f3b7ad9d0107782bbef5004477d
ArtLegends <norik.artemka@gmail.com>
2023-09-17 16:54:03 +0300

    .

--- monitor.js
+++ monitor.js
@@ -1,3 +1,325 @@
-function redirectToPreorder() {
-    window.location.href = "https://tm-market.com/en/products/asus-rog-swift-oled-pg48uq";
+function determineLocation() {
+    const userLang = (navigator.language || navigator.userLanguage).substring(0,2);
+    return translations[userLang] ? userLang : 'en';
 }
+
+var translations = {
+    "de": {
+        "meta": {
+            "description": "Bestellen Sie den Asus ROG Swift OLED PG48UQ Gaming-Monitor mit einem Rabatt von 30% mit dem Promo-Code TECH30. Erfahren Sie mehr über die neuesten Spezifikationen und Funktionen.",
+            "keywords": "Asus ROG Swift OLED PG48UQ, Gaming-Monitor, TechMarket, jetzt bestellen, Promo-Code",
+            "title": "ROG Swift OLED PG48UQ bestellen"
+        },
+        "content": {
+            "h1": "Asus ROG Swift OLED PG48UQ",
+            "h3_preorder": "Bestellen Sie den Asus ROG Swift OLED PG48UQ bei TechMarket.",
+            "p_description": "Der Asus ROG Swift OLED PG48UQ Monitor ist darauf ausgelegt, ein unvergleichliches Spielerlebnis zu bieten. Sein OLED-Display sorgt für lebendige Farben und tiefe Schwarzwerte. Genießen Sie eine hohe Bildwiederholfrequenz und minimale Eingabeverzögerungen und tauchen Sie tief in die Welt des Gamings ein.",
+            "button_order": "Jetzt bestellen"
+        },
+        "images": {
+            "photo_2023-08-06_15-42-25 (81).jpg": "Asus ROG Swift OLED PG48UQ Gaming-Monitor",
+            "photo_2023-08-06_15-42-25 (84).jpg": "Asus ROG Swift OLED PG48UQ Gaming-Monitor"
+        },
+        "specifications": {
+            "Display": {
+                "Bildschirmgröße (Zoll)": "47.53",
+                "Panel-Typ": "OLED",
+                "Tatsächliche Auflösung": "3840x2160",
+                "Bildwiederholfrequenz (max)": "138Hz",
+                "Seitenverhältnis": "16:9",
+                "Bildschirmoberfläche": "Blendfrei"
+            },
+            "Videoeigenschaften": {
+                "Spurfreie Technologie": "Ja",
+                "GamePlus": "Ja",
+                "Game Visual": "Ja",
+                "HDR-Modus": "Ja"
+            },
+            "I/O-Anschlüsse": {
+                "USB-Hub": "4x USB 3.2 Gen 1 Typ-A",
+                "HDMI (v2.0)": "x 2",
+                "HDMI (v2.1)": "x 2",
+                "DisplayPort 1.4 DSC": "x 1"
+            },
+            "Audiofunktionen": {
+                "Lautsprecher": "Ja(10Wx2)",
+                "Subwoofer": "Ja(15W)"
+            },
+            "Stromverbrauch": {
+                "Stromverbrauch": "<300W",
+                "Energiesparmodus": "<0.5W",
+                "Ausschaltmodus": "<0.3W",
+                "Spannung": "100-240V, 50/60Hz"
+            },
+            "Mechanisches Design": {
+                "1/4\" Stativgewinde": "Ja",
+                "Neigung": "Ja (+5° ~ -4°)",
+                "Höhenverstellung": "Nein",
+                "VESA-Wandmontage": "300x300mm",
+                "Kensington-Schloss": "Ja"
+            }
+        },
+        "promo_content": {
+            "h3_1": "Warum Asus ROG Swift OLED PG48UQ?",
+            "p1": "Entdecken Sie den Asus ROG Swift OLED PG48UQ Gaming-Monitor, der ein unvergleichliches Spielerlebnis bieten soll. Mit seinem OLED-Display genießen Sie lebendige Farben und tiefe Schwarztöne. Tauchen Sie mit schnellen Bildwiederholraten und geringer Eingabeverzögerung in die Action ein. Steigern Sie Ihr Gaming-Setup mit diesem hochmodernen Monitor.",
+            "h3_2": "Warum bei TechMarket bestellen?",
+            "p2": "Bei TechMarket sorgen wir dafür, dass Sie zu den Ersten gehören, die die neuesten Geräte in den Händen halten. Profitieren Sie von exklusiven Vorbestellervorteilen, erstklassigem Kundenservice und einem nahtlosen Einkaufserlebnis. Außerdem erhalten Sie besondere Rabatte und Angebote, die Sie nirgendwo anders finden!",
+            "button_order": "Jetzt bestellen"
+        }
+    },
+    "en": {
+        "meta": {
+            "description": "Order the Asus ROG Swift OLED PG48UQ Gaming Monitor with a 30% discount using the promo code TECH30. Learn more about the latest specifications and features.",
+            "keywords": "Asus ROG Swift OLED PG48UQ, Gaming Monitor, TechMarket, order now, Promo-Code",
+            "title": "Order ROG Swift OLED PG48UQ"
+        },
+        "content": {
+            "h1": "Asus ROG Swift OLED PG48UQ",
+            "h3_preorder": "Order the Asus ROG Swift OLED PG48UQ at TechMarket.",
+            "p_description": "The Asus ROG Swift OLED PG48UQ monitor is designed to offer an unparalleled gaming experience. Its OLED display delivers vibrant colors and deep black levels. Enjoy high refresh rates and minimal input lag, and immerse yourself deeply in the world of gaming.",
+            "button_order": "Order now"
+        },
+        "images": {
+            "photo_2023-08-06_15-42-25 (81).jpg": "Asus ROG Swift OLED PG48UQ Gaming Monitor",
+            "photo_2023-08-06_15-42-25 (84).jpg": "Asus ROG Swift OLED PG48UQ Gaming Monitor"
+        },
+        "specifications": {
+            "Display": {
+                "Screen Size (inches)": "47.53",
+                "Panel Type": "OLED",
+                "Actual Resolution": "3840x2160",
+                "Refresh Rate (max)": "138Hz",
+                "Aspect Ratio": "16:9",
+                "Screen Surface": "Non-glare"
+            },
+            "Video Features": {
+                "Trace Free Technology": "Yes",
+                "GamePlus": "Yes",
+                "Game Visual": "Yes",
+                "HDR Mode": "Yes"
+            },
+            "I/O Ports": {
+                "USB Hub": "4x USB 3.2 Gen 1 Type-A",
+                "HDMI (v2.0)": "x 2",
+                "HDMI (v2.1)": "x 2",
+                "DisplayPort 1.4 DSC": "x 1"
+            },
+            "Audio Features": {
+                "Speakers": "Yes(10Wx2)",
+                "Subwoofer": "Yes(15W)"
+            },
+            "Power Consumption": {
+                "Power Consumption": "<300W",
+                "Power Save Mode": "<0.5W",
+                "Off Mode": "<0.3W",
+                "Voltage": "100-240V, 50/60Hz"
+            },
+            "Mechanical Design": {
+                "1/4\" Tripod Thread": "Yes",
+                "Tilt": "Yes (+5° ~ -4°)",
+                "Height Adjustment": "No",
+                "VESA Wall Mounting": "300x300mm",
+                "Kensington Lock": "Yes"
+            }
+        },
+        "promo_content": {
+            "h3_1": "Why Asus ROG Swift OLED PG48UQ?",
+            "p1": "Discover the Asus ROG Swift OLED PG48UQ Gaming Monitor that promises to deliver an unparalleled gaming experience. With its OLED display, enjoy vibrant colors and deep black levels. Dive into the action with fast refresh rates and low input lag. Enhance your gaming setup with this cutting-edge monitor.",
+            "h3_2": "Why order from TechMarket?",
+            "p2": "At TechMarket, we ensure you are among the first to get your hands on the latest devices. Benefit from exclusive pre-order advantages, top-notch customer service, and a seamless shopping experience. Also, avail of special discounts and offers that you won't find anywhere else!",
+            "button_order": "Order now"
+        }
+    },
+    "fr": {
+        "meta": {
+            "description": "Commandez le moniteur de jeu Asus ROG Swift OLED PG48UQ avec une réduction de 30% en utilisant le code promo TECH30. Découvrez les dernières spécifications et fonctionnalités.",
+            "keywords": "Asus ROG Swift OLED PG48UQ, Moniteur de jeu, TechMarket, commander maintenant, Code promo",
+            "title": "Commandez le ROG Swift OLED PG48UQ"
+        },
+        "content": {
+            "h1": "Asus ROG Swift OLED PG48UQ",
+            "h3_preorder": "Commandez l'Asus ROG Swift OLED PG48UQ sur TechMarket.",
+            "p_description": "Le moniteur Asus ROG Swift OLED PG48UQ est conçu pour offrir une expérience de jeu inégalée. Son écran OLED offre des couleurs éclatantes et des niveaux de noir profonds. Profitez de taux de rafraîchissement élevés et d'un faible décalage à l'entrée, et plongez profondément dans le monde du jeu.",
+            "button_order": "Commander maintenant"
+        },
+        "images": {
+            "photo_2023-08-06_15-42-25 (81).jpg": "Moniteur de jeu Asus ROG Swift OLED PG48UQ",
+            "photo_2023-08-06_15-42-25 (84).jpg": "Moniteur de jeu Asus ROG Swift OLED PG48UQ"
+        },
+        "specifications": {
+            "Affichage": {
+                "Taille de l'écran (pouces)": "47.53",
+                "Type de panneau": "OLED",
+                "Résolution réelle": "3840x2160",
+                "Taux de rafraîchissement (max)": "138Hz",
+                "Rapport d'aspect": "16:9",
+                "Surface de l'écran": "Anti-reflet"
+            },
+            "Fonctionnalités vidéo": {
+                "Technologie Trace Free": "Oui",
+                "GamePlus": "Oui",
+                "Game Visual": "Oui",
+                "Mode HDR": "Oui"
+            },
+            "Ports I/O": {
+                "Hub USB": "4x USB 3.2 Gen 1 Type-A",
+                "HDMI (v2.0)": "x 2",
+                "HDMI (v2.1)": "x 2",
+                "DisplayPort 1.4 DSC": "x 1"
+            },
+            "Fonctionnalités audio": {
+                "Haut-parleurs": "Oui(10Wx2)",
+                "Caisson de basses": "Oui(15W)"
+            },
+            "Consommation d'énergie": {
+                "Consommation": "<300W",
+                "Mode d'économie d'énergie": "<0.5W",
+                "Mode hors tension": "<0.3W",
+                "Tension": "100-240V, 50/60Hz"
+            },
+            "Conception mécanique": {
+                "Filetage de trépied 1/4\"": "Oui",
+                "Inclinaison": "Oui (+5° ~ -4°)",
+                "Réglage en hauteur": "Non",
+                "Fixation murale VESA": "300x300mm",
+                "Verrou Kensington": "Oui"
+            }
+        },
+        "promo_content": {
+            "h3_1": "Pourquoi Asus ROG Swift OLED PG48UQ?",
+            "p1": "Découvrez le moniteur de jeu Asus ROG Swift OLED PG48UQ qui promet d'offrir une expérience de jeu inégalée. Avec son écran OLED, profitez de couleurs vives et de niveaux de noir profonds. Plongez dans l'action avec des taux de rafraîchissement rapides et un faible décalage à l'entrée. Améliorez votre configuration de jeu avec ce moniteur de pointe.",
+            "h3_2": "Pourquoi commander chez TechMarket?",
+            "p2": "Chez TechMarket, nous veillons à ce que vous soyez parmi les premiers à mettre la main sur les derniers appareils. Bénéficiez d'avantages de précommande exclusifs, d'un service client de premier ordre et d'une expérience d'achat fluide. De plus, profitez de réductions spéciales et d'offres que vous ne trouverez nulle part ailleurs!",
+            "button_order": "Commander maintenant"
+        }
+    },
+    "es": {
+        "meta": {
+            "description": "Ordene el monitor de juego Asus ROG Swift OLED PG48UQ con un 30% de descuento utilizando el código promocional TECH30. Conozca más sobre las últimas especificaciones y características.",
+            "keywords": "Asus ROG Swift OLED PG48UQ, Monitor de juego, TechMarket, ordenar ahora, Código promocional",
+            "title": "Ordene ROG Swift OLED PG48UQ"
+        },
+        "content": {
+            "h1": "Asus ROG Swift OLED PG48UQ",
+            "h3_preorder": "Ordene el Asus ROG Swift OLED PG48UQ en TechMarket.",
+            "p_description": "El monitor Asus ROG Swift OLED PG48UQ está diseñado para ofrecer una experiencia de juego inigualable. Su pantalla OLED ofrece colores vibrantes y niveles de negro profundos. Disfrute de altas tasas de refresco y un retraso de entrada mínimo, y sumérjase profundamente en el mundo del juego.",
+            "button_order": "Ordenar ahora"
+        },
+        "images": {
+            "photo_2023-08-06_15-42-25 (81).jpg": "Monitor de juego Asus ROG Swift OLED PG48UQ",
+            "photo_2023-08-06_15-42-25 (84).jpg": "Monitor de juego Asus ROG Swift OLED PG48UQ"
+        },
+        "specifications": {
+            "Pantalla": {
+                "Tamaño de pantalla (pulgadas)": "47.53",
+                "Tipo de panel": "OLED",
+                "Resolución real": "3840x2160",
+                "Tasa de refresco (máx)": "138Hz",
+                "Relación de aspecto": "16:9",
+                "Superficie de la pantalla": "Antirreflejo"
+            },
+            "Características de video": {
+                "Tecnología sin rastro": "Sí",
+                "GamePlus": "Sí",
+                "Visualización del juego": "Sí",
+                "Modo HDR": "Sí"
+            },
+            "Puertos I/O": {
+                "Hub USB": "4x USB 3.2 Gen 1 Tipo-A",
+                "HDMI (v2.0)": "x 2",
+                "HDMI (v2.1)": "x 2",
+                "DisplayPort 1.4 DSC": "x 1"
+            },
+            "Funciones de audio": {
+                "Altavoces": "Sí(10Wx2)",
+                "Subwoofer": "Sí(15W)"
+            },
+            "Consumo de energía": {
+                "Consumo de energía": "<300W",
+                "Modo de ahorro de energía": "<0.5W",
+                "Modo apagado": "<0.3W",
+                "Voltaje": "100-240V, 50/60Hz"
+            },
+            "Diseño mecánico": {
+                "Rosca para trípode 1/4\"": "Sí",
+                "Inclinación": "Sí (+5° ~ -4°)",
+                "Ajuste de altura": "No",
+                "Montaje en pared VESA": "300x300mm",
+                "Candado Kensington": "Sí"
+            }
+        },
+        "promo_content": {
+            "h3_1": "¿Por qué Asus ROG Swift OLED PG48UQ?",
+            "p1": "Descubra el monitor de juego Asus ROG Swift OLED PG48UQ que promete ofrecer una experiencia de juego sin igual. Con su pantalla OLED, disfrute de colores vibrantes y niveles de negro profundos. Sumérjase en la acción con tasas de refresco rápidas y un retraso de entrada bajo. Mejore su configuración de juego con este monitor de vanguardia.",
+            "h3_2": "¿Por qué ordenar desde TechMarket?",
+            "p2": "En TechMarket, nos aseguramos de que sea uno de los primeros en tener en sus manos los últimos dispositivos. Benefíciese de ventajas exclusivas de preventa, un servicio al cliente de primera categoría y una experiencia de compra sin complicaciones. Además, aproveche descuentos especiales y ofertas que no encontrará en ningún otro lugar.",
+            "button_order": "Ordenar ahora"
+        }
+    }        
+};
+
+function applyTranslation(lang) {
+    if (!translations[lang]) {
+        console.error(`No translations available for language: ${lang}`);
+        return; 
+    }
+
+    console.log("Lang value:", lang);
+
+    document.querySelector('meta[name="description"]').setAttribute('content', translations[lang].meta.description);
+    if (translations[lang] && translations[lang].meta) {
+        document.querySelector('meta[name="description"]').setAttribute('content', translations[lang].meta.description);
+        document.querySelector('meta[name="keywords"]').setAttribute('content', translations[lang].meta.keywords);
+        document.title = translations[lang].meta.title;
+    } else {
+        console.warn("Translation for the specified language is not available.");
+    }    
+    document.querySelector('meta[name="keywords"]').setAttribute('content', translations[lang].meta.keywords);
+    document.title = translations[lang].meta.title;
+    document.querySelector('img[src="photo_2023-08-06_15-42-25 (81).jpg"]').setAttribute("alt", translations[lang].images["photo_2023-08-06_15-42-25 (81).jpg"]);
+    let imgElement = document.querySelector('img[src="photo_2023-08-06_15-42-25 (81).jpg"]');
+    if (imgElement) {
+        imgElement.setAttribute("alt", translations[lang].images["photo_2023-08-06_15-42-25 (81).jpg"]);
+    }
+
+    document.querySelectorAll('img[alt="Bild von MacBook Air 15"]').forEach(img => {
+        img.setAttribute('alt', translations[lang].images.macbook_air_15);
+    });
+
+    document.querySelector('h1').textContent = translations[lang].content.h1;
+    document.querySelector('#preorderContent h3').textContent = translations[lang].content.h3_preorder;
+    document.querySelector('#preorderContent p').textContent = translations[lang].content.p_description;
+    document.querySelector('#preorderContent button').textContent = translations[lang].content.button_order;
+
+    document.querySelector('.promo-content h3:nth-child(1)').textContent = translations[lang].promo_content.h3_1;
+    document.querySelector('.promo-content h3:nth-child(3)').textContent = translations[lang].promo_content.h3_2;
+    document.querySelector('.promo-content h3').textContent = translations[lang].promo_content.h3_2;
+    document.querySelector('.promo-content p:nth-child(2)').textContent = translations[lang].promo_content.p1;
+    document.querySelector('.promo-content h3').textContent = translations[lang].promo_content.h3;
+    document.querySelector('.promo-content p:nth-child(4)').textContent = translations[lang].promo_content.p2;
+    document.querySelector('.promo-content button').textContent = translations[lang].promo_content.button_order;
+
+    var specSections = document.querySelectorAll('.specs-section');
+    var specCategories = Object.keys(translations[lang].specifications);
+
+    specSections.forEach((section, idx) => {
+        var specCategory = specCategories[idx];
+        var specKeys = Object.keys(translations[lang].specifications[specCategory]);
+        specKeys.forEach(function(key, index) {
+            section.querySelector(`.spec-row:nth-child(${index + 1}) .spec-value`).textContent = translations[lang].specifications[specCategory][key];
+        });
+    });
+}
+
+window.onload = function() {
+    const userLang = determineLocation();
+    applyTranslation(userLang);
+    
+    // Устанавливаем выбранным язык пользователя в выпадающем списке
+    document.getElementById('languageSelector').value = userLang;
+}
+
+
+document.getElementById('languageSelector').addEventListener('change', function() {
+    applyTranslation(this.value);
+});
